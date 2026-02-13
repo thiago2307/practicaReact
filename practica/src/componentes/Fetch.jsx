e7b7764d0cc4b4367848af9749df54eb6f928251
thiago2307 <tasartore@escuelasproa.edu.ar>
2026-02-13 17:01:55 -0300

    Fetch api

---
+++ practica/src/componentes/Fetch.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Posts;
